9d6e0d220736ddba00ca19c91010a6582dd55a2e
Aleksandra Löytäinen <aleksandra.loytainen@innofactor.com>
2023-03-14 14:35:05 +0200

    Added count for all feedback

--- unicafe/src/App.js
+++ unicafe/src/App.js
@@ -11,23 +11,27 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const [feedbackTotal, setFeedbackTotal] = useState(0)
 
   const feedbackGood = () => {
     setGood(good + 1)
     const updatedGood = good + 1
     setGood(updatedGood)
+    setFeedbackTotal(updatedGood + neutral + bad)
   }
 
   const feedbackNeutral = () => {
     setNeutral(neutral + 1)
     const updatedNeutral = neutral + 1
     setNeutral(updatedNeutral)
+    setFeedbackTotal(updatedNeutral + good + bad)
   }
 
   const feedbackBad = () => {
     setBad(bad  + 1)
     const updatedBad = bad + 1
     setBad(updatedBad)
+    setFeedbackTotal(updatedBad + good + neutral)
   }
 
   return (
@@ -41,6 +45,7 @@ const App = () => {
       <p>Good: {good}</p>
       <p>Neutral: {neutral}</p>
       <p>Bad: {bad}</p>
+      <p>All: {feedbackTotal}</p>
     </div>
   )
 }
