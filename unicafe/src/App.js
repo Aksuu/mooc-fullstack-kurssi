4bd22067e4be33e68634ebaea6fd0834c5097720
Aleksandra Löytäinen <aleksandra.loytainen@innofactor.com>
2023-03-14 12:05:41 +0200

    Created basic feedbacksystem for Unicafe

--- unicafe/src/App.js
+++ unicafe/src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react'
 
-function App() {
+const Button = ({ handleClick, text }) => (
+  <button onClick={handleClick}>
+    {text}
+  </button>
+)
+
+const App = () => {
+  // tallenna napit omaan tilaansa
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  const feedbackGood = () => {
+    setGood(good + 1)
+    const updatedGood = good + 1
+    setGood(updatedGood)
+  }
+
+  const feedbackNeutral = () => {
+    setNeutral(neutral + 1)
+    const updatedNeutral = neutral + 1
+    setNeutral(updatedNeutral)
+  }
+
+  const feedbackBad = () => {
+    setBad(bad  + 1)
+    const updatedBad = bad + 1
+    setBad(updatedBad)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Give feedback</h1>
+      <Button handleClick={feedbackGood} text= 'Good' />
+      <Button handleClick={feedbackNeutral} text= 'Neutral' />
+      <Button handleClick={feedbackBad} text= 'Bad' />
+
+      <h1>Statistics</h1>
+      <p>Good: {good}</p>
+      <p>Neutral: {neutral}</p>
+      <p>Bad: {bad}</p>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
