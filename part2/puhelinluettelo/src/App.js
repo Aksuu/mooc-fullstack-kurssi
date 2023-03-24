96e4f796c97a517aa18b951e02a06511434cdfa5
Aleksandra Löytäinen <aleksandra.loytainen@innofactor.com>
2023-03-24 14:16:54 +0200

    Exercise 2.6

--- part2/puhelinluettelo/src/App.js
+++ part2/puhelinluettelo/src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react'
 
-function App() {
+const Person = ({ person }) => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <p>{person.content}</p>
+  )
 }
 
-export default App;
+const App = () => {
+  const [persons, setPersons] = useState([
+    { name: 'Aksu' }
+  ])
+  const [newName, setNewName] = useState('')
+  const [showAll, setShowAll] = useState(true)
+
+  const addName = (event) => {
+    event.preventDefault()
+    const nameObject = {
+    content: newName,
+    id: persons.length + 1,
+  }
+    setPersons(persons.concat(nameObject))
+    setNewName('')
+}
+
+  const handleNameChange = (event) => {
+    console.log(event.target.value)
+    setNewName(event.target.value)
+  }
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={addName}>
+        <div>
+          name: <input
+                  value={newName}
+                  onChange={handleNameChange}
+                />
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      {persons.map(person => <Person key={person.id} person={person}/>)}
+      <div>debug: {newName}</div>
+      <ul>
+        {persons.name}
+      </ul>
+    </div>
+  )
+
+}
+
+export default App
